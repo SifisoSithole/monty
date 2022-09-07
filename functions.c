c05560fe2264650d668f86be2da89584328cbdca
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-07 15:06:24 +0200

    Add: pop

--- functions.c
+++ functions.c
@@ -15,7 +15,7 @@ int pop(stack_t **stack, unsigned int line_number)
 
 	if (!*stack)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		return (-1);
 	}
 	h = (**stack).next;
